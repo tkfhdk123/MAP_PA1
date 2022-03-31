1a71d98bacba73ba6cfad1af2ef551a074c7babc
Jay <tkfhdk123@gmail.com>
2022-03-31 13:17:48 +0900

    Second commit

--- app/src/main/java/edu/skku/cs/pa1/MainActivity.java
+++ app/src/main/java/edu/skku/cs/pa1/MainActivity.java
@@ -3,12 +3,22 @@ package edu.skku.cs.pa1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView1;
+    private ListView listView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText=(EditText) findViewById(R.id.edittext);
+        String input=editText.getText().toString();
+
+
+
     }
 }
