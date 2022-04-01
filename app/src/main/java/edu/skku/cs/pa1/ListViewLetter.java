b9d702c2442934eebc6c6b4ab9f1495ce6930953
Jay <tkfhdk123@gmail.com>
2022-04-01 22:20:55 +0900

    Desktop final commit

--- app/src/main/java/edu/skku/cs/pa1/ListViewLetter.java
+++ app/src/main/java/edu/skku/cs/pa1/ListViewLetter.java
@@ -1,38 +1,66 @@
 package edu.skku.cs.pa1;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ListViewLetter extends BaseAdapter {
     private Context mContext;
     private ArrayList<String> items;
+    private ArrayList<String> items2;
 
-    public ListViewLetter(Context mContext, ArrayList<String> items){
+    public ListViewLetter(Context mContext, ArrayList<String> items, ArrayList<String> items2){
         this.mContext=mContext;
         this.items=items;
+        this.items2=items2;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return items.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return items.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if(view==null){
+            LayoutInflater layoutinflater=(LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view=layoutinflater.inflate(R.layout.listview_letter, viewGroup, false);
+        }
+        TextView tv_lt=view.findViewById(R.id.textview_lt);
+
+        String text=items.get(i).toString();
+        tv_lt.setText(text.toUpperCase());
+
+        String color=items2.get(i).toString();
+        if(color.equals("green")){
+            tv_lt.setBackgroundColor(Color.parseColor("#99F691"));
+            tv_lt.setTextColor(Color.parseColor("#000000"));
+        }
+        else if(color.equals("yellow")){
+            tv_lt.setBackgroundColor(Color.parseColor("#FFE46F"));
+            tv_lt.setTextColor(Color.parseColor("#000000"));
+        }
+        else if(color.equals("gray")){
+            tv_lt.setBackgroundColor(Color.parseColor("#787C7E"));
+            tv_lt.setTextColor(Color.parseColor("#FFFFFF"));
+        }
+
+        return view;
     }
 }
--- app/src/main/java/edu/skku/cs/pa1/MainActivity.java
+++ app/src/main/java/edu/skku/cs/pa1/MainActivity.java
@@ -21,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
     private ListView listview_yellow;
     private ListView listview_green;
     private ListViewWord listviewword;
+    private ListViewLetter listviewletter_gray;
+    private ListViewLetter listviewletter_yellow;
+    private ListViewLetter listviewletter_green;
     private ArrayList<String> items;
     private ArrayList<String> items2;
     private ArrayList<String> gray;
+    private ArrayList<String> gray2;
     private ArrayList<String> yellow;
+    private ArrayList<String> yellow2;
     private ArrayList<String> green;
+    private ArrayList<String> green2;
     private String answer;
     private int num;
     private int retry;
@@ -41,8 +47,11 @@ public class MainActivity extends AppCompatActivity {
         items=new ArrayList<>();
         items2=new ArrayList<>();
         gray=new ArrayList<>();
+        gray2=new ArrayList<>();
         yellow=new ArrayList<>();
+        yellow2=new ArrayList<>();
         green=new ArrayList<>();
+        green2=new ArrayList<>();
 
         try {
             InputStream is=getApplicationContext().getAssets().open("wordle_words.txt");
@@ -62,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
             EditText editText=(EditText)findViewById(R.id.edittext);
             String input=editText.getText().toString();
             input=input.toLowerCase();
-            btn.setText(answer);
 
             boolean check=false;
             for(int i=0; i<num; i++){
@@ -75,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Word '" + input + "' not in dictionary!", Toast.LENGTH_SHORT).show();
             }
             else{
+                editText.setText("");
                 listview_w=findViewById(R.id.lv_word);
                 items.add(input);
                 items2.add(answer);
@@ -92,27 +101,50 @@ public class MainActivity extends AppCompatActivity {
 
                 for(int l=0; l<5; l++){
                     if(parse_input[l].equals(parse_answer[l])){
-                        if(yellow.contains(parse_input[l]))
+                        if(yellow.contains(parse_input[l])) {
                             yellow.remove(parse_input[l]);
-                        if(green.contains(parse_input[l])==false)
+                        }
+                        if(green.contains(parse_input[l]) == false) {
                             green.add(parse_input[l]);
+                        }
                     }
                     else{
-                        boolean wd_in=false;
-                        for(int o=0; o<5; o++){
-                            if(parse_input[l].equals(parse_answer[o]))
-                                wd_in=true;
-                        }
-                        if(wd_in==false)
-                            if(gray.contains(parse_input[l])==false)
-                                gray.add(parse_input[l]);
-                        else
-                            if(green.contains(parse_input[l])==false)
-                                if(yellow.contains(parse_input[l])==false)
+                        if(answer.contains(parse_input[l])) {
+                            if (green.contains(parse_input[l]) == false) {
+                                if (yellow.contains(parse_input[l]) == false) {
                                     yellow.add(parse_input[l]);
+                                }
+                            }
+                        }
+                        else {
+                            if (gray.contains(parse_input[l]) == false) {
+                                gray.add(parse_input[l]);
+                            }
+                        }
                     }
                 }
 
+                listview_gray=findViewById(R.id.lv_letter_gray);
+                listview_yellow=findViewById(R.id.lv_letter_yellow);
+                listview_green=findViewById(R.id.lv_letter_green);
+
+                for(int x=0; x<gray.size(); x++){
+                    gray2.add("gray");
+                }
+                for(int y=0; y<yellow.size(); y++){
+                    yellow2.add("yellow");
+                }
+                for(int z=0; z<green.size(); z++){
+                    green2.add("green");
+                }
+
+                listviewletter_gray=new ListViewLetter(MainActivity.this, gray, gray2);
+                listviewletter_yellow=new ListViewLetter(MainActivity.this, yellow, yellow2);
+                listviewletter_green=new ListViewLetter(MainActivity.this, green, green2);
+
+                listview_gray.setAdapter(listviewletter_gray);
+                listview_yellow.setAdapter(listviewletter_yellow);
+                listview_green.setAdapter(listviewletter_green);
             }
         });
     }
